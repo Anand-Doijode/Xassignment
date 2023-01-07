58a0410d0865d4a05966a72a86aa7db6703853bc
Anand Doijode <ananddoijode8@gmail.com>
2023-01-07 18:43:41 +0530

    Optional parameters added

--- handlers/job.handler.js
+++ handlers/job.handler.js
@@ -3,15 +3,20 @@ const Job = require('../models/job');
 
 module.exports = {
   addJob: async (req, res) => {
-    const { title, description, email } = req.body;
+    const {
+      title, description, email, requiredSkills, expLevel,
+    } = req.body;
+
     const payload = {
       title,
       description,
       email,
+      requiredSkills,
+      expLevel,
     };
     try {
-      const job = new Job(payload);
-      job.save();
+
+      Job.create(payload);
       return StandardResponse.success(res, {});
     } catch (error) {
       StandardResponse.failure(res, error);
