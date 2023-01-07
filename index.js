f12a382fed273599685bcee92a17bb167f5cf576
Anand Doijode <ananddoijode8@gmail.com>
2023-01-07 14:13:33 +0530

    Initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const Express = require('express');
+const { xss } = require('express-xss-sanitizer');
+
+const app = new Express();
+const port = process.env.PORT;
+
+app.use(xss());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
+module.exports = app;
